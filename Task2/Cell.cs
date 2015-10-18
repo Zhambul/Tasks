62c30c910e81456950139dbbead43457b7702f6d
Zhambul <zhambul-96@mail.ru>
2015-10-18 04:55:42 +0300

    patch

---
+++ Task2/Cell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Cell
+    {
+        public int Value { get; set; }
+        public int Size { get; set; }
+        public int StartIndex { get; set; }
+
+        public Cell(int value, int size)
+        {
+            Value = value;
+            Size = size;
+        }
+    }
+}
